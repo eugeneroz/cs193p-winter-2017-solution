8cb665fecd18abed728f39dfc3c4e5f67dd36a39
Eugene Rozenberg <rozenbergeugene@gmail.com>
2017-10-03 00:45:42 +0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Eugene Rozenberg on 03/10/2017.
+//  Copyright © 2017 Eugene Rozenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
